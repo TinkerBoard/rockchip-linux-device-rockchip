f534be8118e1e68f01fda591051d091cd5cfdc1a
Rock Shen <rock_shen@asus.com>
2023-01-13 16:16:56 +0800

    build.sh: merge from rk3399_linux4.19_release_v1.2.2_20221220
    9ea12b2 build.sh: update rk_sign_tool usage for v1.3.6
    7db9ef2 Fix stucking at repo manifest command
    384baef build.sh: fix the cross_compile for arm
    819dead build.sh: update rk_sign_tool usage
    
    Change-Id: I16218a3ad336f642669395aef7434c0e71281c9b
    Signed-off-by: Rock Shen <rock_shen@asus.com>

--- tinker_board_2/build.sh
+++ tinker_board_2/build.sh
@@ -469,7 +469,7 @@ function build_check_cross_compile(){
 	case $RK_ARCH in
 	arm|armhf)
 		if [ -d "$TOP_DIR/prebuilts/gcc/linux-x86/arm/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf" ]; then
-			CROSS_COMPILE=$(realpath $TOP_DIR)/prebuilts/gcc/linux-x86/arm/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/bin/arm-linux-gnueabihf-
+			CROSS_COMPILE=$(realpath $TOP_DIR)/prebuilts/gcc/linux-x86/arm/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-
 		export CROSS_COMPILE=$CROSS_COMPILE
 		fi
 		;;
@@ -1215,6 +1215,8 @@ function build_save(){
 	.repo/repo/repo forall -c \
 		"$TOP_DIR/device/rockchip/common/gen_patches_body.sh"
 
+	#Copy stubs
+	yes | .repo/repo/repo manifest -r -o $STUB_PATH/manifest_$RELEASE_NAME.xml
 	mkdir -p $STUB_PATCH_PATH/kernel
 	cp kernel/.config $STUB_PATCH_PATH/kernel
 	cp kernel/vmlinux $STUB_PATCH_PATH/kernel
@@ -1236,9 +1238,6 @@ function build_save(){
 	echo "KERNEL: defconfig: $RK_KERNEL_DEFCONFIG, dts: $RK_KERNEL_DTS" >> $STUB_PATH/build_cmd_info
 	echo "BUILDROOT: $RK_CFG_BUILDROOT" >> $STUB_PATH/build_cmd_info
 
-	#Copy stubs
-	.repo/repo/repo manifest -r -o $STUB_PATH/manifest_$RELEASE_NAME.xml
-
 	finish_build
 }
 
@@ -1259,10 +1258,7 @@ function create_keys() {
 	test -d u-boot/keys && echo "ERROR: u-boot/keys has existed" && return -1
 
 	mkdir u-boot/keys -p
-	cd u-boot/keys
-	$TOP_DIR/rkbin/tools/rk_sign_tool kk --bits 2048
-	cd -
-
+	./rkbin/tools/rk_sign_tool kk --bits 2048 --out u-boot/keys
 	ln -s private_key.pem u-boot/keys/dev.key
 	ln -s public_key.pem u-boot/keys/dev.pubkey
 	openssl req -batch -new -x509 -key u-boot/keys/dev.key -out u-boot/keys/dev.crt
